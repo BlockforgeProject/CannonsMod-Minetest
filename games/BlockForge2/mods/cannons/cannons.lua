a872a80dbfe348c62cdce4f3c82be0f0d1ff4f78
Ricky <riccardo@forkforge.net>
2013-01-06 19:37:31 +0100

    v1.0

--- games/BlockForge2/mods/cannons/cannons.lua
+++ games/BlockForge2/mods/cannons/cannons.lua
@@ -1,3 +1,20 @@
+--  _______  _______  _        _        _______  _        _______ 
+-- (  ____ \(  ___  )( (    /|( (    /|(  ___  )( (    /|(  ____ \
+-- | (    \/| (   ) ||  \  ( ||  \  ( || (   ) ||  \  ( || (    \/
+-- | |      | (___) ||   \ | ||   \ | || |   | ||   \ | || (_____ 
+-- | |      |  ___  || (\ \) || (\ \) || |   | || (\ \) |(_____  )
+-- | |      | (   ) || | \   || | \   || |   | || | \   |      ) |
+-- | (____/\| )   ( || )  \  || )  \  || (___) || )  \  |/\____) |
+-- (_______/|/     \||/    )_)|/    )_)(_______)|/    )_)\_______)
+--                                                                                    
+-- Author: Riccardo B. <riccardo@cryptolab.net>
+-- License: Gnu GPL v3
+-- Description: Small mod for adding cannons to minetest.
+-- Homepage: http://forkforge.net/proj.13
+-- Requirements: https://github.com/BlockforgeProject/BlockForge-2-/
+--
+--
+-- Thanks to LorenzoVulcan (https://github.com/BlockforgeProject/) for Crafting Recipes
 
 -- Cannon 1
 local cannon1 = {
